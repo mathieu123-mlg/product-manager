624857bb5aaa20aef0ea2324a7c3c1fe3615bb4a
mathieu <mathieu.user24@gmail.com>
2025-12-06 15:52:14 +0300

    feat: Initial commit

---
+++ src/main/java/td/evaluation/td1jdbc/Td1JdbcApplication.java
@@ -0,0 +1,13 @@
+package td.evaluation.td1jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td1JdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/td/evaluation/td1jdbc/Td1JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package td.evaluation.td1jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
