847601f97e5af62b80816b6539ffd854cb8abde3
mathieu <mathieu.user24@gmail.com>
2025-12-06 17:46:08 +0300

    feat: add Class Category and Product

---
+++ src/main/java/td/evaluation/td1jdbc/Category.java
@@ -0,0 +1,37 @@
+package td.evaluation.td1jdbc;
+
+import java.util.Objects;
+
+public class Category {
+    private final int id;
+    private final String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "\nCategory{" +
+                "\n\tid=" + id +
+                ", name='" + name + '\'' +
+                "\n}";
+    }
+}
---
+++ src/main/java/td/evaluation/td1jdbc/Product.java
@@ -0,0 +1,44 @@
+package td.evaluation.td1jdbc;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Product {
+    private final int id;
+    private final String name;
+    private final Instant creationDatetime;
+    private final Category category;
+
+    public Product(int id, String name, Instant creationDatetime, Category category) {
+        this.id = id;
+        this.name = name;
+        this.creationDatetime = creationDatetime;
+        this.category = category;
+    }
+
+    public String getCategoryName() {
+        return this.category.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(name, product.name) && Objects.equals(creationDatetime, product.creationDatetime) && Objects.equals(category, product.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, creationDatetime, category);
+    }
+
+    @Override
+    public String toString() {
+        return "\nProduct{" +
+                "\n\tid=" + id +
+                ", name='" + name + '\'' +
+                ", creationDatetime=" + creationDatetime +
+                ", category=" + category +
+                "\n}";
+    }
+}
